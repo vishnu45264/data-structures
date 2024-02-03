66b1b17a7f2aebf01a5f7d6bfd1e259b55a96c6b
vishnu45264 <157686401+vishnu45264@users.noreply.github.com>
2024-02-03 11:59:22 +0530

    Add files via upload

---
+++ duplicate.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,sum=0;
+	printf("enter array size = ");
+	scanf("%d",&n);
+    int a[n];
+    printf("enter array elements\n");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    	for(j=i+1;j<n;j++)
+    	{
+    		if(a[i]==a[j]&&i!=j)
+    		{
+    	       sum =sum+1;
+            	break;
+    		}
+		}
+	}
+	 printf("Total no of duplicate elements = %d",sum);
+
+	return 0;
+}
