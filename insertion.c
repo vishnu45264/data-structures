18a145aefcac4c51b1876340cca6b3dba742c431
vishnu45264 <157686401+vishnu45264@users.noreply.github.com>
2024-02-03 12:05:10 +0530

    Add files via upload

---
+++ insertion.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,x,v;
+	printf("enter array size =");
+	scanf("%d",&n);
+    int a[n];
+    printf("enter array elements \n");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+     printf("array elements before insertion\n");
+    for(i=0;i<n;i++)
+    {
+    printf("%d\t",a[i]);
+    }
+    printf("\nenter position of elememt");
+    scanf("%d",&x);
+    printf("\nenter number");
+	scanf("%d",&v);
+	for(i=n;i>=x;i--)
+	{
+		a[i+1]=a[i];
+	}
+	a[x]=v;
+	 printf("array elements after insertion\n");
+    for(i=1;i<n+1;i++)
+    {
+    printf("%d\t",a[i]);
+}
+    return 0;
+}
+    
